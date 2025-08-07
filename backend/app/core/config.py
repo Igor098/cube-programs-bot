e3cef032eb453810870c6f58405f278538abcf35
Игорь Новожилов <igor.novozhilov2018@gmail.com>
2025-08-07 16:36:45 +0300

    feat: сделал основу для бота

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,24 +4,24 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DB_HOST: str = None
-    DB_PORT: str = None
-    DB_NAME: str = None
-    DB_USER: str = None
-    DB_PASSWORD: str = None
-    REDIS_HOST: str = None
-    REDIS_PORT: str = None
-    REDIS_USER: str = None
-    REDIS_PASSWORD: str = None
-    ACCESS_TTL: int = None
-    REFRESH_TTL: int = None
-    CSRF_TTL: int = None
-    BOT_TTL: int = None
-    ALGORITHM: str = None
-    SECRET_KEY:str = None
-    JWT_ISSUER: str = None
-    JWT_AUDIENCE: str = None
-    MODE: str = None
+    DB_HOST: str = ""
+    DB_PORT: str = ""
+    DB_NAME: str = ""
+    DB_USER: str = ""
+    DB_PASSWORD: str = ""
+    REDIS_HOST: str = ""
+    REDIS_PORT: str = ""
+    REDIS_USER: str = ""
+    REDIS_PASSWORD: str = ""
+    ACCESS_TTL: int = 0
+    REFRESH_TTL: int = 0
+    CSRF_TTL: int = 0
+    BOT_TTL: int = 0
+    ALGORITHM: str = ""
+    SECRET_KEY:str =""
+    JWT_ISSUER: str = ""
+    JWT_AUDIENCE: str = ""
+    MODE: str = ""
 
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../", ".env")
